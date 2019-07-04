a0d8e69c46b1a38ce3aa2d02489a160226b1c7ed
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-07-04 14:28:51 +0100

    Update provisining to make token's last for 2 weeks

--- provision/roles/token_generator_ldap/files/load_env.sh
+++ provision/roles/token_generator_ldap/files/load_env.sh
@@ -2,5 +2,6 @@
 
 export JWT_PRIVATE_KEY_FILE=/home/webapp/id_rsa
 export JWT_PUBLIC_KEY_FILE=/home/webapp/id_rsa.pub
+export JWT_ACCESS_TOKEN_EXPIRES=1210000
 export FLASK_APP="/home/webapp/token-generator-ldap/app.py"
 export FLASK_CONFIG_FILE="/home/webapp/production.cfg"
